31f7b9d356e7939acafe9b3da72fd8c4c5c07009
phili <philippegueorguievskii@gmail.com>
2025-12-17 00:20:07 +0100

    Ran formatter

--- main.cu
+++ main.cu
@@ -13,26 +13,24 @@
 #define NTPB 1024
 
 /// Helper for catching errors
-void testCUDA(cudaError_t error, const char* file, int line) {
-
+void testCUDA(cudaError_t error, const char *file, int line) {
 	if (error != cudaSuccess) {
 		printf("There is an error in file %s at line %d\n", file, line);
 		exit(EXIT_FAILURE);
 	}
 }
 
-// Has to be defined in the compilation in order to get the correct value of the 
+// Has to be defined in the compilation in order to get the correct value of the
 // macros __FILE__ and __LINE__
 #define testCUDA(error) (testCUDA(error, __FILE__ , __LINE__))
 
-__global__ void init_curand_state_k(curandStateXORWOW_t* states)
-{
+__global__ void init_curand_state_k(curandStateXORWOW_t *states) {
 	int id = threadIdx.x + blockIdx.x * blockDim.x;
-	curand_init(0,id,0,&states[id]);
+	curand_init(0, id, 0, &states[id]);
 }
 
-__device__ float calculate_price(float param_sigma, float param_a, float starting_r, float starting_time, float target_time, float time_delta, curandStateXORWOW_t* states)
-{
+__device__ float calculate_price(float param_sigma, float param_a, float starting_r, float starting_time,
+                                 float target_time, float time_delta, curandStateXORWOW_t *states) {
 	curandStateXORWOW_t localState = states[threadIdx.x + blockIdx.x * blockDim.x];
 
 	int total_steps = (target_time - starting_time) / time_delta;
@@ -41,8 +39,7 @@ __device__ float calculate_price(float param_sigma, float param_a, float startin
 	float integral = 0;
 	float r_step = starting_r;
 
-	for(int step = 1; step <= total_steps; step++)
-	{
+	for (int step = 1; step <= total_steps; step++) {
 		float r_prev_step = r_step;
 
 		float step_time = starting_time + step * time_delta;
@@ -55,22 +52,22 @@ __device__ float calculate_price(float param_sigma, float param_a, float startin
 
 			float A, B;
 			// Second term of the mean (formula for the integral)
-			if (step_time < 5 || previous_step_time >= 5)
-			{	
+			if (step_time < 5 || previous_step_time >= 5) {
 				A = (step_time < 5) ? 0.012 : 0.019;
 				B = (step_time < 5) ? 0.0014 : 0.001;
-				step_time = (step_time < 5) ? step_time : step_time-5;
-				m += 
-					(
-						param_a * (A + B * step_time) - B
-						- expf(-param_a * time_delta) * (param_a * (A + B * (step_time - time_delta) - B))
-					) / (param_a * param_a);
-			}else{
-				float d_1 = 5-previous_step_time;
-				float d_2 = step_time-5;
+				step_time = (step_time < 5) ? step_time : step_time - 5;
+				m +=
+				(
+					param_a * (A + B * step_time) - B
+					- expf(-param_a * time_delta) * (param_a * (A + B * (step_time - time_delta) - B))
+				) / (param_a * param_a);
+			} else {
+				float d_1 = 5 - previous_step_time;
+				float d_2 = step_time - 5;
 				A = 0.012;
 				B = 0.0014;
-				m += 1 / (param_a * param_a) * (param_a * (A + B * 5) - B - expf(-param_a * d_1) * (param_a * (A + B * previous_step_time) - B)) * expf(-param_a * d_2);
+				m += 1 / (param_a * param_a) * (param_a * (A + B * 5) - B - expf(-param_a * d_1) * (
+					                                param_a * (A + B * previous_step_time) - B)) * expf(-param_a * d_2);
 
 				A = 0.019;
 				B = 0.001;
@@ -117,147 +114,140 @@ __global__ void tree_sum(CallableValue val, float *out) {
 
 // Theta functions not tested
 
-__device__ float theta(float* f,float sigma,float t,float a,float dt, int maxindex)
-{
-	int s = roundf(t/dt);
-	float result = a*f[s] + (sigma*sigma*(1-expf(-2*a*t))/(2*a)); 
-	if(s > 0 && s < maxindex)
-		return result + (f[s+1] - f[s-1])/(2*dt);
-	
-	if(s == 0)
-		return result + (f[s+1] - f[s])/(dt);
-	if(s == maxindex)
-		return result + (f[s] - f[s-1])/(dt);
+__device__ float theta(float *f, float sigma, float t, float a, float dt, int maxindex) {
+	int s = roundf(t / dt);
+	float result = a * f[s] + sigma * sigma * (1 - expf(-2 * a * t)) / (2 * a);
+	if (s > 0 && s < maxindex)
+		return result + (f[s + 1] - f[s - 1]) / (2 * dt);
+
+	if (s == 0)
+		return result + (f[s + 1] - f[s]) / dt;
+	if (s == maxindex)
+		return result + (f[s] - f[s - 1]) / dt;
 	return -1;
 }
 
-__global__ void theta_k(float* f,float sigma,float a,float dt, int maxindex,float* res)
-{
-	int tid = threadIdx.x + blockDim.x*blockIdx.x;
+__global__ void theta_k(float *f, float sigma, float a, float dt, int maxindex, float *res) {
+	int tid = threadIdx.x + blockDim.x * blockIdx.x;
 
-	if(tid <= maxindex)
-	{
-		float t = tid*dt;
-		res[tid] = theta(f,sigma,t,a,dt,maxindex);
+	if (tid <= maxindex) {
+		float t = tid * dt;
+		res[tid] = theta(f, sigma, t, a, dt, maxindex);
 	}
 }
 
-__global__ void ZBC_k(float S1,float S2,float K,float* f,float* p,float* theta,float sigma,float dt,float a, float rs,curandStateXORWOW_t* states,float *ZBC)
-{
+__global__ void ZBC_k(float S1, float S2, float K, float *f, float *p, float *theta, float sigma, float dt, float a,
+                      float rs, curandStateXORWOW_t *states, float *ZBC) {
 	// Calculate A and B to get P
 	// Simulate r, and obtain the integral (can use function of before with a minor tweak)
 	// put everything together and do reduction as usual
 	int tid = threadIdx.x;
 	extern __shared__ float sdata[];
-	curandStateXORWOW_t localState = states[threadIdx.x + blockIdx.x*blockDim.x];
+	curandStateXORWOW_t localState = states[threadIdx.x + blockIdx.x * blockDim.x];
 	// convert S1 and S2 to the closest step.
-	int S1step = roundf(S1/dt);
-	int S2step = roundf(S2/dt);
-	float B = (1-expf(-a*(S2-S1)))/a;
-	float A = p[S2step]/p[S1step] * expf( B * f[S1step] - (sigma*sigma *(1-expf(-2*a*S2)))/(4*a) * B * B);
+	int S1step = roundf(S1 / dt);
+	int S2step = roundf(S2 / dt);
+	float B = (1 - expf(-a * (S2 - S1))) / a;
+	float A = p[S2step] / p[S1step] * expf(B * f[S1step] - (sigma * sigma * (1 - expf(-2 * a * S2))) / (4 * a) * B * B);
 	//printf("%d %d %f %f %f \n",S2step,S1step,p[S2step],p[S1step],f[S1step]);
 	float rS1;
 	// for P I still miss r(S1). Lets calculate integral of r(t). We are gonna calculate r(S1) during it.
 
-	float sintegral = dt/2*rs;
+	float sintegral = dt / 2 * rs;
 
 	// maybe not elegant but for now it will suffice
-	int X = S1/dt;
+	int X = S1 / dt;
 	float r_temp = rs;
-	for(int f = 1; f <= X; f++)
-	{
+	for (int f = 1; f <= X; f++) {
 		float integral = 0;
 		float2 G = curand_normal2(&localState);
-		integral += expf(-a*(dt)) * theta[f-1]; 
+		integral += expf(-a * (dt)) * theta[f - 1];
 		integral += 1.0f * theta[f];
-		integral *= dt/2;
+		integral *= dt / 2;
 
-		float m = r_temp * expf(-a*(dt)) + integral;
-		float sigmaBig = sqrtf(sigma*sigma*(1-expf(-2*a*(dt)))/(2*a));
-		r_temp = m + sigmaBig*G.x;
-		if(f == X) rS1 = r_temp;
-		sintegral += dt/2 * r_temp * ((f != X) ? 2 : 1);
+		float m = r_temp * expf(-a * (dt)) + integral;
+		float sigmaBig = sqrtf(sigma * sigma * (1 - expf(-2 * a * (dt))) / (2 * a));
+		r_temp = m + sigmaBig * G.x;
+		if (f == X) rS1 = r_temp;
+		sintegral += dt / 2 * r_temp * ((f != X) ? 2 : 1);
 	}
-	float P = A*expf(-B*rS1);
-	sdata[tid] = expf(-sintegral)*fmaxf(0.0f,P-K);
+	float P = A * expf(-B * rS1);
+	sdata[tid] = expf(-sintegral) * fmaxf(0.0f, P - K);
 	__syncthreads();
 
-	for(int k = blockDim.x/2; k > 0; k /= 2)
-	{
-		if(tid < k){
-			sdata[tid] += sdata[k+tid];
+	for (int k = blockDim.x / 2; k > 0; k /= 2) {
+		if (tid < k) {
+			sdata[tid] += sdata[k + tid];
 		}
 		__syncthreads();
 	}
 
-	if(tid == 0){
+	if (tid == 0) {
 		atomicAdd(ZBC, sdata[0]);
 	}
 }
 
-__global__ void ZBC_derivative_k(float S1,float S2,float K,float* f,float* p,float* theta,float sigma,float dt,float a, float rs,curandStateXORWOW_t* states,float *ZBC)
-{
+__global__ void ZBC_derivative_k(float S1, float S2, float K, float *f, float *p, float *theta, float sigma, float dt,
+                                 float a, float rs, curandStateXORWOW_t *states, float *ZBC) {
 	// derivative of P(S_1,S_2)
 	// we need: derivative of A and derivative of r
 	int tid = threadIdx.x;
 	extern __shared__ float sdata[];
-	curandStateXORWOW_t localState = states[threadIdx.x + blockIdx.x*blockDim.x];
+	curandStateXORWOW_t localState = states[threadIdx.x + blockIdx.x * blockDim.x];
 	// need to calculate P(S_1,S_2) otherwise I cant decide which path to take
-	int S1step = roundf(S1/dt);
-	int S2step = roundf(S2/dt);
-	float B = (1-expf(-a*(S2-S1)))/a;
-	float A = p[S2step]/p[S1step] * expf( B * f[S1step] - (sigma*sigma *(1-expf(-2*a*S2)))/(4*a) * B * B);
+	int S1step = roundf(S1 / dt);
+	int S2step = roundf(S2 / dt);
+	float B = (1 - expf(-a * (S2 - S1))) / a;
+	float A = p[S2step] / p[S1step] * expf(B * f[S1step] - (sigma * sigma * (1 - expf(-2 * a * S2))) / (4 * a) * B * B);
 	float rS1;
 
 	// calculates integral of r_s from 0 to S_1
-	int X = S1/dt;
+	int X = S1 / dt;
 	float r_temp = rs;
 	float temp_dev = 0;
 	float dev_integral = 0;
-	float sintegral = dt/2*rs;
-	for(int f = 1; f <= X; f++)
-	{
+	float sintegral = dt / 2 * rs;
+	for (int f = 1; f <= X; f++) {
 		float2 G = curand_normal2(&localState);
 		float integral = 0;
 
-		integral += expf(-a*(dt)) * theta[f-1]; 
+		integral += expf(-a * (dt)) * theta[f - 1];
 		integral += 1.0f * theta[f];
-		integral *= dt/2;
+		integral *= dt / 2;
 
-		float m = r_temp * expf(-a*(dt)) + integral;
-		float sigmaBig = sqrtf(sigma*sigma*(1-expf(-2*a*(dt)))/(2*a));
-		r_temp = m + sigmaBig*G.x;
-		if(f == X) rS1 = r_temp;
-		sintegral += dt/2 * r_temp * ((f != X) ? 2 : 1);
+		float m = r_temp * expf(-a * (dt)) + integral;
+		float sigmaBig = sqrtf(sigma * sigma * (1 - expf(-2 * a * (dt))) / (2 * a));
+		r_temp = m + sigmaBig * G.x;
+		if (f == X) rS1 = r_temp;
+		sintegral += dt / 2 * r_temp * ((f != X) ? 2 : 1);
 
-		float w = f*dt;
-		float m_dev = temp_dev * expf(-a*(dt)) + (2*sigma*expf(-a*w)*(cosh(a*w)-cosh(a*(w-dt))))/(a*a);
-		temp_dev = m_dev + sigmaBig/sigma * G.x;
-		dev_integral += dt/2*temp_dev*((f != X) ? 2 : 1);
+		float w = f * dt;
+		float m_dev = temp_dev * expf(-a * (dt)) + (2 * sigma * expf(-a * w) * (cosh(a * w) - cosh(a * (w - dt)))) / (
+			              a * a);
+		temp_dev = m_dev + sigmaBig / sigma * G.x;
+		dev_integral += dt / 2 * temp_dev * ((f != X) ? 2 : 1);
 	}
-	float P = A*expf(- B*rS1);
+	float P = A * expf(-B * rS1);
 
 
-	float result = fmaxf(0.0f,(P-K));
-	result *= dev_integral*expf(-sintegral);	
+	float result = fmaxf(0.0f, (P - K));
+	result *= dev_integral * expf(-sintegral);
 
-	if(P > K)
-	{
+	if (P > K) {
 		float Adev = 0;
-		Adev = A*(-(2*sigma*(1-expf(-2*a*S2)))/(4*a)*B*B);
-		float Pdev = A*expf(-B*rS1)*(-B)*temp_dev + Adev*expf(-B*rS1);
-		sdata[tid] = Pdev*expf(-sintegral)-result;
-	}else{sdata[tid] = -result;}
+		Adev = A * (-(2 * sigma * (1 - expf(-2 * a * S2))) / (4 * a) * B * B);
+		float Pdev = A * expf(-B * rS1) * (-B) * temp_dev + Adev * expf(-B * rS1);
+		sdata[tid] = Pdev * expf(-sintegral) - result;
+	} else { sdata[tid] = -result; }
 	__syncthreads();
-	for(int k = blockDim.x/2; k > 0; k /= 2)
-	{
-		if(tid < k){
-			sdata[tid] += sdata[k+tid];
+	for (int k = blockDim.x / 2; k > 0; k /= 2) {
+		if (tid < k) {
+			sdata[tid] += sdata[k + tid];
 		}
 		__syncthreads();
 	}
 
-	if(tid == 0){
+	if (tid == 0) {
 		atomicAdd(ZBC, sdata[0]);
 	}
 }
@@ -268,25 +258,24 @@ int main() {
 
 	float param_sigma = 0.1;
 	float param_a = 1.0;
-	float T_start = 0; 
+	float T_start = 0;
 	float T_end = 10;
 	float delta_T = (T_end - T_start) / (steps - 1);
 
-	curandStateXORWOW_t* states;
+	curandStateXORWOW_t *states;
 
 	cudaMalloc(&states,NB * NTPB * sizeof(curandStateXORWOW_t));
 	init_curand_state_k<<<NB, NTPB>>>(states);
 	cudaDeviceSynchronize();
 
-	float *F,*theta;
-	cudaMallocManaged(&F, steps*sizeof(float));
-	cudaMallocManaged(&theta, steps*sizeof(float));
+	float *f;
+	cudaMallocManaged(&f, steps * sizeof(float));
 
 	float *sum;
 	cudaMallocManaged(&sum, sizeof(float));
 
 	float avg_prev;
-	F[0] = 0.012;
+	f[0] = 0.012;
 	for (int i = 0; i < steps; ++i) {
 		float T_i = T_start + delta_T * i;
 		*sum = 0.0f;
@@ -304,7 +293,7 @@ int main() {
 		// f(0, T)
 		if (i != 0) {
 			float delta = logf(avg) - logf(avg_prev);
-			F[i] = -(delta / delta_T);
+			f[i] = -(delta / delta_T);
 			//std::cout << "f(0, " << T_i << ") = " << F[i] << std::endl;
 		}
 
@@ -312,14 +301,18 @@ int main() {
 	}
 
 	//Question 2.1
-	theta_k<<<1,steps>>>(F,param_sigma,param_a,delta_T,steps-1,theta);
+	float *theta;
+	cudaMallocManaged(&theta, steps * sizeof(float));
+
+	theta_k<<<1, steps>>>(f, param_sigma, param_a, delta_T, steps - 1, theta);
 	cudaDeviceSynchronize();
-	for(int i = 0; i <= steps; i++)
-		printf("%f %f \n",T_start+delta_T*i,theta[i]);
+
+	for (int i = 0; i <= steps; i++)
+		printf("%f %f \n", T_start + delta_T * i, theta[i]);
 
 	cudaFree(sum);
 	cudaFree(states);
-	cudaFree(F);
+	cudaFree(f);
 	cudaFree(theta);
 
 	return 0;
@@ -331,63 +324,61 @@ int main_old() {
 	float sigma = 0.1;
 	float s = 0;
 	float T = 10;
-	float dt =  ((float)T)/((float)steps);
+	float dt = ((float) T) / ((float) steps);
 	float rzero = 0.012;
 	float a = 1.0;
-	float* PGPU; 
-	float* FGPU;
-	float* thetaGPU;
-	float* ZBCGPU;
-	float* ZBCGPUD;
+	float *PGPU;
+	float *FGPU;
+	float *thetaGPU;
+	float *ZBCGPU;
+	float *ZBCGPUD;
 	float ZBC;
 	float ZBC2;
 	int num_el = steps + 1;
-	float* P = (float*)malloc(sizeof(float)*num_el);
-	float* F = (float*)malloc(sizeof(float)*num_el);
+	float *P = (float *) malloc(sizeof(float) * num_el);
+	float *F = (float *) malloc(sizeof(float) * num_el);
 
-	cudaMalloc(&PGPU,sizeof(float));
-	cudaMalloc(&thetaGPU,sizeof(float)*num_el);
+	cudaMalloc(&PGPU, sizeof(float));
+	cudaMalloc(&thetaGPU, sizeof(float) * num_el);
 	cudaMemset(PGPU, 0, sizeof(float));
-	curandStateXORWOW_t* states;
-	cudaMalloc(&states,n*sizeof(curandStateXORWOW_t));
-	cudaMalloc(&ZBCGPU,sizeof(float));
-	cudaMalloc(&ZBCGPUD,sizeof(float));
-	cudaMemset(ZBCGPU,0,sizeof(float));
-	cudaMemset(ZBCGPUD,0,sizeof(float));
+	curandStateXORWOW_t *states;
+	cudaMalloc(&states, n * sizeof(curandStateXORWOW_t));
+	cudaMalloc(&ZBCGPU, sizeof(float));
+	cudaMalloc(&ZBCGPUD, sizeof(float));
+	cudaMemset(ZBCGPU, 0, sizeof(float));
+	cudaMemset(ZBCGPUD, 0, sizeof(float));
 
 	init_curand_state_k<<<NB, NTPB>>>(states);
 	cudaDeviceSynchronize();
 
-	float PCPU,FCPU;
+	float PCPU, FCPU;
 	P[0] = 1;
 	F[0] = rzero;
-	for(int i = 1; i <= steps; i++)
-	{
-		float t = dt*i;
+	for (int i = 1; i <= steps; i++) {
+		float t = dt * i;
 		std::cout << sigma << " " << a << " " << rzero << " " << s << " " << t << " " << dt << std::endl;
 		tree_sum<<<NB, NTPB, NTPB * sizeof(float)>>>([sigma, a, rzero, s, t, dt, states] __device__ {
-			return calculate_price(sigma, a,rzero,s,t,dt, states);
+			return calculate_price(sigma, a, rzero, s, t, dt, states);
 		}, PGPU);
 		//Bond_Price_k<<<NB,NTPB,2*NTPB*sizeof(float)>>>(sigma,dt,a,rzero,s,t,states,PGPU);
 		cudaDeviceSynchronize();
 		cudaMemcpy(&PCPU, PGPU, sizeof(float), cudaMemcpyDeviceToHost);
 
 		std::cout << "sum : " << PCPU << std::endl;
-    	P[i] = PCPU/n;
+		P[i] = PCPU / n;
 		std::cout << i << " : " << t << ", " << P[i] << std::endl;
 
-    	cudaMemset(PGPU,0,sizeof(float));
+		cudaMemset(PGPU, 0, sizeof(float));
 	}
 
-	for(int i = 1; i <= num_el; i++) 
-	{
-    	F[i] = -(logf(P[i]) - logf(P[i-1]))/dt;
+	for (int i = 1; i <= num_el; i++) {
+		F[i] = -(logf(P[i]) - logf(P[i - 1])) / dt;
 	}
-	cudaMalloc(&FGPU,sizeof(float)*num_el);
-	cudaMalloc(&PGPU,sizeof(float)*num_el);
-	cudaMemcpy(FGPU,F,sizeof(float)*num_el,cudaMemcpyHostToDevice);
-	cudaMemcpy(PGPU,P,sizeof(float)*num_el,cudaMemcpyHostToDevice);
-	theta_k<<<1,num_el>>>(FGPU,sigma,a,dt,steps,thetaGPU);
+	cudaMalloc(&FGPU, sizeof(float) * num_el);
+	cudaMalloc(&PGPU, sizeof(float) * num_el);
+	cudaMemcpy(FGPU, F, sizeof(float) * num_el, cudaMemcpyHostToDevice);
+	cudaMemcpy(PGPU, P, sizeof(float) * num_el, cudaMemcpyHostToDevice);
+	theta_k<<<1,num_el>>>(FGPU, sigma, a, dt, steps, thetaGPU);
 
 	// float S1,float S2,float K,float* f,float* p,float* theta,float sigma,float dtstep,float dt,float a, float rs,curandStateXORWOW_t* states,float *ZBC)
 	/*ZBC_k<<<NB,NTPB,NTPB*sizeof(float)>>>(5,10,expf(-0.1),FGPU,PGPU,thetaGPU,sigma,dt,a,rzero,states,ZBCGPU);
